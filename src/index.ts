7ca72f5f66ce416855eb7317667d91242e00e3d1
JPCalsavara <jpcalsavara@gmail.com>
2025-05-13 09:27:59 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World")
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => console.log(`Listen port ${port}`));
